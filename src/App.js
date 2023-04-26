92f68c0fddb592d55ef4a6931af3faa556412e10
Aditya Srivastava <kallakallz123@gmail.com>
2023-04-26 15:17:01 +0530

    Added navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import ResponsiveAppBar from './components/Navbar';
+import Home from './components/Home';
+import Shop from './components/Shop';
+import Blog from './components/Blog';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ResponsiveAppBar/>
+    <Routes>
+      <Route exact path='/home' element = {<Home/>}/>
+      <Route exact path='/shop' element = {<Shop/>}/>
+      <Route exact path='/blog' element = {<Blog/>}/>
+    </Routes>
+    </>
   );
 }
 
