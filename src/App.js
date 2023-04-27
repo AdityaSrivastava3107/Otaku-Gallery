33032248b629fcbccfeb0f87b1ce41c6c5fc2c75
Aditya Srivastava <kallakallz123@gmail.com>
2023-04-28 02:20:36 +0530

    Added daisyUI and worked on frontend a bit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import ResponsiveAppBar from './components/Navbar';
 import Home from './components/Home';
 import Shop from './components/Shop';
 import Blog from './components/Blog';
+import Appbar from './components/Appbar';
 
 function App() {
   return (
     <>
-    <ResponsiveAppBar/>
+    <Appbar/>
     <Routes>
       <Route exact path='/home' element = {<Home/>}/>
       <Route exact path='/shop' element = {<Shop/>}/>
